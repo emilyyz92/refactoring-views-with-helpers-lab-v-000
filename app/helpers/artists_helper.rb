4ca08eb8f337d6ad234ffaa877d484e5f52243f6
Emily Zhao <tong.zhao.92@gmail.com>
2018-07-16 18:40:46 +0000

    automatically backed up by learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,8 +1,9 @@
 module ArtistsHelper
   def display_artist(song)
     if song.artist.songs != []
-      link_to artist_path(artist)
+      link_to artist_path(song.artist)
     else
-      link
+      link_to song_path(song)
+    end
   end
 end
