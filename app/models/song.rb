96fe8139b9eb13c58d145480bd27e7d45e64988b
Emily Zhao <tong.zhao.92@gmail.com>
2018-07-16 18:30:54 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -6,7 +6,7 @@ class Song < ActiveRecord::Base
   end
 
   def artist_name=(name)
-    self.artist = Artist.create(name: name)
+    self.artist = Artist.find_or_create_by(name: name)
     self.artist.name
   end
 end
